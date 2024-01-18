44f167f76ac801a0684e69fd004925f30fd24d28
elgharbi <elgharbiay@gmail.com>
2024-01-18 21:48:11 +0100

    create user

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create user table
+-- attributes: id, email, name
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+)
