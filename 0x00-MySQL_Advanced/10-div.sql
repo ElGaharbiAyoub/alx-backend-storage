98043ff2b7bf9042ef21bde49d801d3aefbd7177
elgharbi <elgharbiay@gmail.com>
2024-01-19 00:19:15 +0100

    fix

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,7 +1,8 @@
 -- script that creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0 if the second number is equal to 0.
-
-CREATE OR REPLACE FUNCTION SafeDiv(a INT, b INT) 
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT) 
 RETURNS INT AS $$
 BEGIN
     IF b = 0 THEN
@@ -11,4 +12,4 @@ BEGIN
     END IF;
 END;
 $$
-LANGUAGE plpgsql;
+DELIMITER;
