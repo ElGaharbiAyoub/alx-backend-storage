fe32d14d815d96fd696c887e7d97bd0085948ebd
elgharbi <elgharbiay@gmail.com>
2024-01-23 21:29:33 +0100

    Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Changes all topics of a school document based on the name """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of a school document based on the name """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
