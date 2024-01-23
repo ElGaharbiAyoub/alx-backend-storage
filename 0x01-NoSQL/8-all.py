8a14294b8cd885a06fffa93036670bf8d88c03b9
elgharbi <elgharbiay@gmail.com>
2024-01-23 17:54:08 +0100

    List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" List all documents in Python """
+import pymongo
+
+
+def list_all(mongo_collection):
+    """ Lists all documents in a collection """
+    return mongo_collection.find()
