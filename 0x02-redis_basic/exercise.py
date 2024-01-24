aae4cc9f3703fd631f2e8ed49d1446c2d5135b33
elgharbi <elgharbiay@gmail.com>
2024-01-24 20:42:22 +0100

    cache redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Exercise for Redis"""
+import redis
+import uuid
+from typing import Union, Callable, Optional
+
+
+class Cache:
+    """ claass for redis cache """
+
+    def __init__(self):
+        """ constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get data from redis """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ get string data from redis """
+        return self.get(key, str)
+    
+    def get_int(self, key: str) -> int:
+        """ get int data from redis """
+        return self.get(key, int)
