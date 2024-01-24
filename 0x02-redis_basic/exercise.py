91a49a4284a27e5ef282a3b1db98749449aa0fb0
elgharbi <elgharbiay@gmail.com>
2024-01-24 23:33:21 +0100

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,6 +17,18 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """ call history decorator """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ wrapper function """
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+    return wrapper
+
 class Cache:
     """ claass for redis cache """
 
@@ -26,6 +38,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store data in redis """
         key = str(uuid.uuid4())
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -26,9 +26,24 @@
 #     assert cache.get(key, fn=fn) == value
 
 
-# =============== task 2 ================
+# =============== task 2 - Incrementing values================
 
 
+
+# """ Main file """
+
+# Cache = __import__('exercise').Cache
+
+# cache = Cache()
+
+# cache.store(b"first")
+# print(cache.get(cache.store.__qualname__))
+
+# cache.store(b"second")
+# cache.store(b"third")
+# print(cache.get(cache.store.__qualname__))
+
+# ========== task 3 - Storing lists ===========
 
 """ Main file """
 
@@ -36,9 +51,15 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-cache.store(b"first")
-print(cache.get(cache.store.__qualname__))
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
 
-cache.store(b"second")
-cache.store(b"third")
-print(cache.get(cache.store.__qualname__))
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
